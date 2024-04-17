b2fa08bb29294e2b374f33c1112a1cdf226ecdbc
Younghun Ko <koyo@koyo.kr>
2024-04-17 10:19:34 +0900

    chore: 백엔드 프로젝트 생성 (#1)

---
+++ backend/src/main/java/com/ssafy/vinopener/VinopenerApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.vinopener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinopenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinopenerApplication.class, args);
+	}
+
+}
