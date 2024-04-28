84f7b8a8e2b8be72255ef010e3139a99cce47259
phc <phc5403@gmail.com>
2024-04-29 06:25:10 +0900

    chore: 마신 날짜 null 수용 여부 수정 (#34)
    마신 날짜를 null로 입력 했을 경우, 현재 시각으로 치환하는 문제를 수정함.

--- backend/src/main/java/com/ssafy/vinopener/domain/cellar/data/dto/request/CellarUpdateRequest.java
+++ backend/src/main/java/com/ssafy/vinopener/domain/cellar/data/dto/request/CellarUpdateRequest.java
@@ -5,8 +5,6 @@ import java.time.LocalDate;
 public record CellarUpdateRequest(
         LocalDate finishedDate
 
-//        @Schema(type = "string", example = TimeFormatConfig.LOCAL_DATE_TIME_EXAMPLE)
-//        LocalDateTime createdTime
 ) {
 
 }
