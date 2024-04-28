bc7535419e366c2e79bb138e202018ca8e1526f4
양희승 <heeseung0223@naver.com>
2024-04-29 01:47:32 +0900

    TEMP: 검색화면(문자) - UI 구현 (#20)
    검색바 CSS 수정중

--- frontend/lib/screens/search/search_text_screen.dart
+++ frontend/lib/screens/search/search_text_screen.dart
@@ -1,33 +1,91 @@
 import 'package:flutter/material.dart';
 
+// constants
+import 'package:frontend/constants/colors.dart';
+
 class SearchTextScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: <Widget>[
-        // 검색바를 상단에 고정
-        SliverAppBar(
-          pinned: true,  // 상단에 고정
-          title: TextField(
-            decoration: InputDecoration(
-              hintText: '검색',
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 3), // 좌우 패딩
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.orange, width: 2),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 4,
+                  offset: Offset(0, 2), // 그림자의 위치 조정
+                ),
+              ],
+              borderRadius: BorderRadius.circular(30), // 테두리 둥글게
+            ),
+            child: TextField(
+              autofocus: true,  // 텍스트 상자에 자동 포커스 되면서 키보드 켜짐
+              decoration: InputDecoration(
+                hintText: '와인 검색',
+                prefixIcon: Icon(Icons.search), // 검색 아이콘
+                suffixIcon: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min, // 아이콘들을 끝쪽에 붙이기 위해
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.camera_alt),
+                          onPressed: () {
+                            // 카메라 아이콘 동작 구현
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.close),
+                          onPressed: () {
+                            // 닫기 아이콘 동작 구현
+                          },
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                border: InputBorder.none, // 텍스트 필드 테두리 제거
+              ),
             ),
           ),
-        ),
-        // 동적으로 변하는 리스트
-        SliverList(
-          delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-              // 여기서 API 데이터를 가지고 각 아이템을 구성합니다.
-              // 예를 들어, searchResults는 API로부터 받은 검색 기록 리스트
-              return ListTile(
-                // title: Text(searchResults[index]),
-              );
-            },
-            // childCount: searchResults.length,  // 검색 결과의 수에 따라 리스트 길이 결정
+          Text(
+            '최근 검색',
           ),
-        ),
-      ],
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Column(
+                    children: [
+                      SliverPadding(
+                        padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                        sliver: SliverList(
+                          delegate: SliverChildListDelegate([
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                // 최근 검색 내역 등 나머지 위젯들 구현
+                              ],
+                            ),
+                          ]),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
