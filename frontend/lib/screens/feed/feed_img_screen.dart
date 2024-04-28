0d77597569dd7fceb1bb625dbb9d1e1d167b165f
최지웅 <jwchoi42@naver.com>
2024-04-28 15:42:16 +0900

    feat: 피드 이미지 첨부 기능 구현, 피드 내용 화면 디자인, 마이페이지 설정 화면 디자인, 세부 페이지 이동 구현, 피드 위젯 구현 (#33)
    Close #33

--- frontend/lib/screens/feed/feed_img_screen.dart
+++ frontend/lib/screens/feed/feed_img_screen.dart
@@ -1,10 +1,56 @@
+import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/constants/colors.dart';
+import 'package:frontend/constants/fonts.dart';
 import 'package:frontend/screens/feed/feed_content_screen.dart';
+import 'package:image_picker/image_picker.dart';
 
-class FeedImageScreen extends StatelessWidget {
+class FeedImageScreen extends StatefulWidget {
   const FeedImageScreen({super.key});
+
+  @override
+  State<FeedImageScreen> createState() => _FeedImageScreenState();
+}
+
+class _FeedImageScreenState extends State<FeedImageScreen> {
+  File? _imageFile;
+  final _imagePicker = ImagePicker();
+
+  Future pickImageFromGallery() async {
+    final pickedImage =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _imageFile = File(pickedImage!.path);
+    });
+  }
+
+  Future showOptions() async {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (context) => CupertinoActionSheet(
+        actions: [
+          CupertinoActionSheetAction(
+            child: Text(
+              '사진첩',
+              style: TextStyle(
+                color: AppColors.primary,
+                fontSize: AppFontSizes.mediumSmall,
+              ),
+            ),
+            onPressed: () {
+              // close the options modal
+              Navigator.of(context).pop();
+              // get image from gallery
+              pickImageFromGallery();
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,22 +58,72 @@ class FeedImageScreen extends StatelessWidget {
       Navigator.push(
         context,
         CupertinoPageRoute(
-          builder: (context) => FeedContentScreen(),
+          builder: (context) => FeedContentScreen(
+            imageFile: _imageFile,
+          ),
         ),
       );
     }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Feed Image'),
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: Icon(Icons.close),
+        ),
+        title: Center(
+          child: Text(
+            '이미지 업로드',
+            style: TextStyle(
+              fontSize: AppFontSizes.mediumSmall,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
         actions: [
           TextButton(
             onPressed: navigateNext,
-            child: Text('다음'),
+            child: Text(
+              '다음',
+              style: TextStyle(
+                fontSize: AppFontSizes.medium,
+                color: AppColors.primary,
+              ),
+            ),
           ),
         ],
       ),
-      body: Center(child: Text('feed image')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 20),
+          _imageFile == null
+              ? Image.asset(
+                  'assets/images/wine.jpg',
+                  width: 400,
+                  height: 400,
+                )
+              : Image.file(
+                  _imageFile!,
+                  width: 400,
+                  height: 400,
+                  fit: BoxFit.cover,
+                ),
+          SizedBox(height: 20),
+          Center(
+            child: TextButton(
+              child: Text(
+                '이미지 선택',
+                style: TextStyle(
+                  fontSize: AppFontSizes.mediumSmall,
+                  color: AppColors.primary,
+                ),
+              ),
+              onPressed: showOptions,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- frontend/lib/screens/feed/feed_screen.dart
+++ frontend/lib/screens/feed/feed_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/constants/fonts.dart';
+import 'package:frontend/models/feed_model.dart';
 import 'package:frontend/screens/feed/feed_img_screen.dart';
+import 'package:frontend/widgets/common/templates/feed_widget.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
@@ -18,16 +21,34 @@ class FeedScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Feed'),
-        actions: [
-          IconButton(
-            color: Colors.black,
-            onPressed: addFeed,
-            icon: Icon(Icons.add),
-          )
-        ],
+          title: Text(
+            'Feed',
+            style: TextStyle(
+              fontSize: AppFontSizes.large,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [
+            IconButton(
+              color: Colors.black,
+              onPressed: addFeed,
+              icon: Icon(Icons.add),
+            )
+          ],
+          shape: Border(bottom: BorderSide(color: Colors.grey))),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+              SizedBox(height: 20),
+              Feed(
+                feedModel: FeedModel.dummy(),
+              ),
+            ],
+          );
+        },
       ),
-      body: Center(child: Text('feed')),
     );
   }
 }
