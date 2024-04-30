418a9e879fa33c1cd9f810b45c2d39f602f8df35
전원빈 <wjssoqls@gachon.ac.kr>
2024-04-30 11:34:15 +0900

    feat: notelist api 제외 개발 완료(#39)

--- frontend/lib/models/note_model.dart
+++ frontend/lib/models/note_model.dart
@@ -5,15 +5,47 @@ class NoteWine {
   final String winery;
   final String country;
 
+  NoteWine.dummy()
+      : id = 1,
+        winery = 'Shafer',
+        name = 'Shafer Hillside Select Cabernet Sauvignon 2019',
+        imageUrl = 'https://picsum.photos/200/200',
+        country = 'korea';
+
   NoteWine(
-      {required this.id, required this.name, required this.imageUrl, required this.winery, required this.country});
+      {required this.id,
+      required this.name,
+      required this.imageUrl,
+      required this.winery,
+      required this.country});
+
+  factory NoteWine.fromJson(Map<String, dynamic> json) {
+    return NoteWine(
+      id: json['id'],
+      name: json['name'],
+      imageUrl: json['imageUrl'],
+      winery: json['winery'],
+      country: json['country'],
+    );
+  }
 }
 
-class WineColor{
+class WineColor {
   final int id;
   final String name;
 
+  WineColor.dummy()
+      : id = 1,
+        name = "밑집색";
+
   WineColor({required this.id, required this.name});
+
+  factory WineColor.fromJson(Map<String, dynamic> json) {
+    return WineColor(
+      id: json['id'],
+      name: json['name'],
+    );
+  }
 }
 
 class Flavour {
@@ -21,15 +53,51 @@ class Flavour {
   final String taste;
 
   Flavour({required this.id, required this.taste});
+
+  factory Flavour.fromJson(Map<String, dynamic> json) {
+    return Flavour(
+      id: json['id'],
+      taste: json['taste'],
+    );
+  }
 }
 
-class NoteList {
+class Note {
   final int id;
-  final NoteWine noteWine;
+  final NoteWine wine;
   final WineColor color;
-  final double rating;
+  final int rating;
   final List<Flavour> flavours;
 
-  NoteList({required this.id, required this.noteWine, required this.color, required this.rating, required this.flavours});
+  Note(
+      {required this.id,
+      required this.wine,
+      required this.color,
+      required this.rating,
+      required this.flavours});
 
+  factory Note.fromJson(Map<String, dynamic> json) {
+    return Note(
+      id: json['id'],
+      wine: NoteWine.fromJson(json['wine']),
+      color: WineColor.fromJson(json['color']),
+      rating: json['rating'],
+      flavours:
+          List<Flavour>.from(json['flavours'].map((x) => Flavour.fromJson(x))),
+    );
+  }
+
+  factory Note.dummy() {
+    return Note(
+        id: 1,
+        wine: NoteWine.dummy(),
+        color: WineColor.dummy(),
+        rating: 5,
+        flavours: [
+          Flavour(id: 1, taste: "딸기"),
+          Flavour(id: 2, taste: "라즈베리"),
+          Flavour(id: 3, taste: "붉은자두"),
+          Flavour(id: 4, taste: "석류"),
+        ]);
+  }
 }
--- frontend/lib/widgets/note/note_list_card_widget.dart
+++ frontend/lib/widgets/note/note_list_card_widget.dart
@@ -1,38 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/constants/colors.dart';
+import 'package:frontend/constants/fonts.dart';
+import 'package:frontend/widgets/common/atoms/nation_flag_widget.dart';
 
-import '../../models/wine_model.dart';
+import '../../models/note_model.dart';
 
-class NoteCard extends StatelessWidget{
-  final Wine wine;
+class NoteCard extends StatelessWidget {
+  final Note note;
 
-  const NoteCard({super.key, required this.wine});
+  const NoteCard({super.key, required this.note});
 
   @override
   Widget build(BuildContext context) {
     double dimension = MediaQuery.of(context).size.width;
 
     return Container(
-      width: dimension*0.9,
-      height: dimension*0.5,
-        decoration: ShapeDecoration(
+      width: dimension * 0.91,
+      height: dimension * 0.5,
+      decoration: ShapeDecoration(
         shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-    ),),
+          side: BorderSide(width: 1, color: Color(0xFFCACACB)),
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
       child: Row(
         children: [
           Container(
-            width: dimension*0.09,
-
+            width: dimension * 0.09,
+            decoration: ShapeDecoration(
+              color: WineColors.wineColorMap[note.color.name],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    bottomLeft: Radius.circular(12)),
+              ),
+            ),
           ),
           Container(
-
+            width: dimension * 0.31,
+            alignment: Alignment.center,
+            child: Image.network(
+              note.wine.imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
           Container(
-            width: dimension*0.5,
+            padding: EdgeInsets.only(left: dimension*0.03),
+            width: dimension * 0.5,
+            decoration: BoxDecoration(
+              border: Border(
+                left: BorderSide(
+                  color: Color(0xFFCACACB), // 왼쪽 선의 색상 설정
+                  width: 1.0, // 선의 두께
+                ),
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      note.wine.winery,
+                      style: TextStyle(fontSize: AppFontSizes.mediumSmall, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      note.wine.name,
+                      style: TextStyle(fontSize: AppFontSizes.small),
+                    ),
+                    Row(
+                      children: [
+                        NationFlag(height: 12, width: 12),
+                        Text(
+                          note.wine.country,
+                          style: TextStyle(fontSize: AppFontSizes.small),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      "향",
+                      style: TextStyle(fontSize: AppFontSizes.mediumSmall),
+                    ),
+                    SizedBox(width: 10), // "향" 텍스트와 다음 요소 사이의 간격 추가
+                    Expanded(
+                      // Expanded를 사용하여 텍스트 넘침을 방지
+                      child: Wrap(
+                        // Row 대신 Wrap 사용
+                        children: note.flavours
+                            .map((flavour) => Padding(
+                                  padding: const EdgeInsets.only(right: 5),
+                                  // 각 향 요소 사이에 오른쪽 패딩 추가
+                                  child: Text(flavour.taste),
+                                ))
+                            .toList(),
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: AppColors.primary,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      note.rating.toString(),
+                      style: TextStyle(
+                          fontSize: AppFontSizes.mediumSmall,
+                          color: AppColors.primary,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )
+              ],
+            ),
           )
         ],
       ),
     );
   }
-
 }
