c857215ec64b11217385dc80a34dfefa5819f61f
hanjs0625 <hanjs0625@gmail.com>
2024-05-02 11:11:44 +0900

    chore: swagger 주석 및 불필요한 코드 삭제, Type 명시 등 리팩토링 (#2)
    Swagger 주석을 추가하고, ResponseEntity<?> 의 ? 타입을 명시하고, 불필요한 코드를 제거하는 등의 리팩토링을 진행했습니다.

--- backend/src/main/java/com/ssafy/vinopener/domain/user/controller/OAuth2Controller.java
+++ backend/src/main/java/com/ssafy/vinopener/domain/user/controller/OAuth2Controller.java
@@ -3,7 +3,6 @@ package com.ssafy.vinopener.domain.user.controller;
 import com.ssafy.vinopener.domain.user.data.entity.UserEntity;
 import com.ssafy.vinopener.domain.user.service.OAuth2Service;
 import com.ssafy.vinopener.global.jwt.JwtProvider;
-import com.ssafy.vinopener.global.oauth2.GoogleClient;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class OAuth2Controller {
 
     private final OAuth2Service oAuth2Service;
-    private final GoogleClient googleClient;
     private final JwtProvider jwtProvider;
 
+    /**
+     * @param token 사용자 구글 프로필 요청을 위한 Access Token
+     * @return vinopener에서 사용할 Access Token과 Refresh Token
+     */
     @PostMapping("/login/google")
-    public ResponseEntity<?> login(@RequestParam("token") String token) {
+    public ResponseEntity<Map<String, String>> login(@RequestParam("token") String token) {
 
         //프로필 정보 요청 및 가입여부 확인 후 유저 정보 return
         UserEntity user = oAuth2Service.loadUser(token);
@@ -38,8 +40,13 @@ public class OAuth2Controller {
         return ResponseEntity.ok(tokens);
     }
 
+    /**
+     * @param accessToken  vinopener Access Token
+     * @param refreshToken vinopener Refresh Token
+     * @return 새로 발급된 Access Token
+     */
     @PostMapping("/refresh")
-    public ResponseEntity<?> refresh(
+    public ResponseEntity<Map<String, String>> refresh(
             @RequestHeader("Authorization") String accessToken,
             @RequestHeader("refresh-token") String refreshToken
     ) {
