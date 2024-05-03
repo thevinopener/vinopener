51463d90df5528e07c619b709c2fcd6e30f11817
전원빈 <wjssoqls@gachon.ac.kr>
2024-05-03 10:18:41 +0900

    feat: 노트 네비게이션 생성 (#36)

--- frontend/lib/screens/note/note_color_screen.dart
+++ frontend/lib/screens/note/note_color_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/note/note_smell_screen.dart';
-import 'package:frontend/widgets/note/note_taste_widget.dart';
 
 
 class NoteColorScreen extends StatelessWidget {
@@ -20,7 +19,7 @@ class NoteColorScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Note Color'),
+        title: Text('Color'),
         actions: [
           IconButton(
             color: Colors.black,
@@ -30,7 +29,7 @@ class NoteColorScreen extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: NoteTaste(acidity: 3.5, alcohol: 2.0, tannin: 3.5,sweetness: 2.5, intensity: 4.0,),
+
       ),
     );
   }
