9a2cea37f96975043592879e115a65c8ad24e168
Younghun Ko <koyo@koyo.kr>
2024-05-03 12:07:13 +0900

    chore: 스프링 배치 프로젝트 생성 (#42)

---
+++ vinopener-batch/src/test/java/coffee/ssafy/vinopenerbatch/VinopenerBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package coffee.ssafy.vinopenerbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinopenerBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
