776d14b147b9ba54b502a8fc234b4f190a5537b0
전원빈 <wjssoqls@gachon.ac.kr>
2024-05-07 17:34:10 +0900

    feat: STT 연동(#36)

--- frontend/lib/widgets/note/note_stt_widget.dart
+++ frontend/lib/widgets/note/note_stt_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:frontend/constants/colors.dart';
 import 'package:speech_to_text/speech_to_text.dart';
+
 
 class SttWidget extends StatefulWidget {
   const SttWidget({Key? key}) : super(key: key);
@@ -11,6 +13,7 @@ class SttWidget extends StatefulWidget {
 
 class _SttWidgetState extends State<SttWidget> {
   final SpeechToText _speech = SpeechToText();
+  final FlutterTts _flutterTts = FlutterTts();  // TTS 인스턴스 생성
   bool _isListening = false;
   String _text = 'Press the button and start speaking';
 
@@ -18,6 +21,7 @@ class _SttWidgetState extends State<SttWidget> {
   void initState() {
     super.initState();
     _initSpeech();
+    _initTts();
   }
 
   void _initSpeech() async {
@@ -28,6 +32,23 @@ class _SttWidgetState extends State<SttWidget> {
     if (!mounted) return;
     setState(() {
       _isListening = available;
+    });
+  }
+
+  void _initTts() {
+    _flutterTts.setLanguage('ko-KR');
+    _flutterTts.setPitch(1.0);
+
+    _flutterTts.setStartHandler(() {
+      print("TTS Start");
+    });
+
+    _flutterTts.setCompletionHandler(() {
+      print("TTS Complete");
+    });
+
+    _flutterTts.setErrorHandler((msg) {
+      print("TTS Error: $msg");
     });
   }
 
@@ -49,11 +70,19 @@ class _SttWidgetState extends State<SttWidget> {
         setState(() {
           _text = result.recognizedWords;
         });
+        _speak(result.recognizedWords);  // STT 결과를 TTS로 출력
       },
-      localeId: 'ko-KR', // 한국어 로케일 설정
+      localeId: 'ko-KR',
     );
   }
 
+  void _speak(String text) async {
+    if (text.isNotEmpty) {
+      var result = await _flutterTts.speak(text);
+      if (result == 1) print("TTS Success"); // Success
+      else print("TTS Failed with code: $result"); // Check the failure
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
