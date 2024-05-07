07e37ea1d80528106afefaf5403387870bd41f34
전원빈 <wjssoqls@gachon.ac.kr>
2024-05-08 01:28:20 +0900

    feat: STT 설정(#36)

--- frontend/lib/widgets/note/note_stt_widget.dart
+++ frontend/lib/widgets/note/note_stt_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:frontend/constants/colors.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
-
 
 class SttWidget extends StatefulWidget {
   final int currentPage;
@@ -14,20 +14,30 @@ class SttWidget extends StatefulWidget {
   _SttWidgetState createState() => _SttWidgetState();
 }
 
-
 class _SttWidgetState extends State<SttWidget> {
   final SpeechToText _speech = SpeechToText();
-  final FlutterTts _flutterTts = FlutterTts();  // TTS 인스턴스 생성
+  final FlutterTts _flutterTts = FlutterTts();
   bool _isListening = false;
   String _text = 'Press the button and start speaking';
   int _currentPage = 0;
+  List<String> titles = ['색상', '향', '맛', '의견'];
 
   @override
   void initState() {
     super.initState();
+    _currentPage = widget.currentPage;
     _initSpeech();
     _initTts();
-    _currentPage = widget.currentPage;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _promptUser();
+    });
+  }
+
+  void _promptUser() async {
+    if (_currentPage < titles.length) {
+      var title = titles[_currentPage];
+       _speak("$title은 어떠합니까?");
+    }
   }
 
   void _initSpeech() async {
@@ -51,6 +61,7 @@ class _SttWidgetState extends State<SttWidget> {
 
     _flutterTts.setCompletionHandler(() {
       print("TTS Complete");
+      _startListening();
     });
 
     _flutterTts.setErrorHandler((msg) {
@@ -58,49 +69,64 @@ class _SttWidgetState extends State<SttWidget> {
     });
   }
 
-  void _startListening() async {
+  void _startListening() {
     if (!_isListening) {
-      bool available = await _speech.initialize(
-        onStatus: (status) => print('STT Status: $status'),
-        onError: (errorNotification) => print('STT Error: $errorNotification'),
-      );
-      if (!available) {
-        setState(() => _isListening = false);
-        print('The user has denied the use of speech recognition.');
-        return;
-      }
-    }
-    setState(() => _isListening = true);
-    _speech.listen(
-      onResult: (result) {
-        setState(() {
-          _text = result.recognizedWords;
-        });
-        if (result.recognizedWords.contains("다음")) {
-          if (_currentPage < 3) {  // 마지막 페이지 전까지
-            int nextPage = _currentPage + 1;
-            widget.onPageChangeRequest(nextPage);
-            setState(() {
-              _currentPage = nextPage;  // 현재 페이지 업데이트
-            });
-          } else {  // 마지막 페이지인 경우
-            _speak("마지막 항목입니다");
-          }
+      _speech.initialize().then((available) {
+        if (available) {
+          setState(() => _isListening = true);
+          _speech.listen(
+              onResult: _handleResult,
+              localeId: 'ko-KR'  // 여기에서 한국어 음성 인식 설정
+          );
         } else {
-          _speak(result.recognizedWords);  // 다른 음성은 TTS로 처리
+          setState(() => _isListening = false);
+          print('The user has denied the use of speech recognition.');
         }
-      },
-      localeId: 'ko-KR',
-    );
+      });
+    } else {
+      _speech.listen(
+          onResult: _handleResult,
+          localeId: 'ko-KR'  // 여기에서 한국어 음성 인식 설정
+      );
+    }
+
+  }
+
+  void _handleResult(SpeechRecognitionResult result) {
+    setState(() {
+      _text = result.recognizedWords;
+    });
+    _analyzeSpeech(result.recognizedWords);
+  }
+
+  void _analyzeSpeech(String text) {
+    if (text.toLowerCase().contains("다음")) {
+      if (_currentPage < titles.length - 1) {
+        int nextPage = _currentPage + 1;
+        widget.onPageChangeRequest(nextPage);
+        setState(() {
+          _currentPage = nextPage;
+        });
+        _promptUser();
+      } else {
+        _speak("마지막 항목입니다");
+      }
+    } else {
+      _speak(text);
+    }
   }
 
   void _speak(String text) async {
     if (text.isNotEmpty) {
       var result = await _flutterTts.speak(text);
-      if (result == 1) print("TTS Success"); // Success
-      else print("TTS Failed with code: $result"); // Check the failure
+      if (result == 1) {
+        print("TTS Success");
+      } else {
+        print("TTS Failed with code: $result");
+      }
     }
   }
+
 
 
   @override
@@ -117,11 +143,7 @@ class _SttWidgetState extends State<SttWidget> {
               style: const TextStyle(fontSize: 24.0, color: AppColors.white),
             ),
           ),
-          ElevatedButton(
-            onPressed: _startListening,
-            child: Icon(_isListening ? Icons.mic : Icons.mic_none),
-          ),
-          Text(_currentPage.toString(), style: TextStyle(color: AppColors.white),),
+          Text("현재 페이지: ${_currentPage + 1}", style: TextStyle(color: AppColors.white)),
         ],
       ),
     );
