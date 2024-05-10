d40cbb2271fe8f0bd961d2eda21638a5a8bcc337
최지웅 <jwchoi42@naver.com>
2024-05-11 03:31:39 +0900

    feat: 테이스팅 노트 레이아웃 수정 (#71)
    Close #71

--- frontend/lib/screens/note/note_opinion_screen.dart
+++ frontend/lib/screens/note/note_opinion_screen.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:frontend/constants/colors.dart';
 import 'package:frontend/constants/fonts.dart';
@@ -9,16 +7,15 @@ import 'package:provider/provider.dart';
 
 import '../../providers/note/note_wine_provider.dart';
 
-
 class NoteOpinionScreen extends StatelessWidget {
   const NoteOpinionScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     Future<void> postNote() async {
       try {
-        final wineId = Provider.of<NoteWineProvider>(context, listen: false).getWine().id;
+        final wineId =
+            Provider.of<NoteWineProvider>(context, listen: false).getWine().id;
         final noteProvider = Provider.of<NoteProvider>(context, listen: false);
         noteProvider.updateNoteProvider(wineId: wineId);
 
@@ -32,31 +29,27 @@ class NoteOpinionScreen extends StatelessWidget {
       }
     }
 
-
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 20,),
-              Text('의견', style: TextStyle(fontSize: AppFontSizes.large),),
-              SizedBox(height: 10,),
-              Text('자유롭게 의견을 적어보세요!', style: TextStyle(fontSize: AppFontSizes.mediumSmall),),
-              SizedBox(height: 30,),
-              NoteOpinion(),
-              SizedBox(height: 20,),
-              TextButton(
-                onPressed: postNote,
-                child: Text("제출", style: TextStyle(color: Colors.white)),
-                style: TextButton.styleFrom(
-                  backgroundColor: AppColors.primary, // 버튼 배경 색상
-                  padding: EdgeInsets.symmetric(horizontal: 30), // 내부 여백
-                ),
-              ),
-            ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 20),
+          Text('의견', style: TextStyle(fontSize: AppFontSizes.large)),
+          SizedBox(height: 10),
+          Text('자유롭게 의견을 적어보세요!',
+              style: TextStyle(fontSize: AppFontSizes.mediumSmall)),
+          SizedBox(height: 30),
+          NoteOpinion(),
+          SizedBox(height: 20),
+          TextButton(
+            onPressed: postNote,
+            child: Text("제출", style: TextStyle(color: Colors.white)),
+            style: TextButton.styleFrom(
+              backgroundColor: AppColors.primary, // 버튼 배경 색상
+              padding: EdgeInsets.symmetric(horizontal: 30), // 내부 여백
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
