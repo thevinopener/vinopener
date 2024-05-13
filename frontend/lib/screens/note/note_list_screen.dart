16f3b4119fde36c811adf5a109323967940ce468
전원빈 <wjssoqls@gachon.ac.kr>
2024-05-13 10:52:12 +0900

    fix: 노트 리스트 새로고침 수정(#36)

--- frontend/lib/screens/note/note_list_screen.dart
+++ frontend/lib/screens/note/note_list_screen.dart
@@ -8,7 +8,6 @@ import '../../models/note_model.dart';
 import '../../widgets/note/note_list_card_widget.dart';
 import 'note_result_screen.dart';
 
-
 class NoteListScreen extends StatefulWidget {
   @override
   _NoteListScreenState createState() => _NoteListScreenState();
@@ -29,11 +28,26 @@ class _NoteListScreenState extends State<NoteListScreen> {
     });
   }
 
+  void viewDetail(int id) {
+    Navigator.push(
+      context,
+      CupertinoPageRoute(
+        builder: (context) => NoteResultScreen(id: id),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tasting Note'),
+        title: Text(
+          'Tasting Note',
+          style: TextStyle(
+            fontSize: AppFontSizes.large,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         actions: [
           IconButton(
             icon: Icon(Icons.add),
@@ -42,29 +56,39 @@ class _NoteListScreenState extends State<NoteListScreen> {
                 context,
                 CupertinoPageRoute(builder: (context) => NoteSearchScreen()),
               );
-              _loadData();  // 노트 추가 후 화면으로 돌아왔을 때 데이터 새로고침
+              _loadData(); // 노트 추가 후 화면으로 돌아왔을 때 데이터 새로고침
             },
           )
         ],
+        shape: Border(bottom: BorderSide(color: Colors.grey)),
       ),
       body: FutureBuilder<List<WineNoteCard>>(
         future: _notesFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return CircularProgressIndicator();
+            return Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
+            return Center(child: Text('Error: ${snapshot.error}'));
           } else if (snapshot.hasData) {
+            final notes = snapshot.data!;
             return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) => NoteCard(wineNoteCard: snapshot.data![index]),
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0), // 각 아이템에 추가 패딩
+                  child: GestureDetector(
+                    onTap: () => viewDetail(notes[index].id),
+                    child: NoteCard(wineNoteCard: notes[index]), // 아이템 카드
+                  ),
+                );
+              },
             );
           } else {
-            return Text('No notes available');
+            return Center(child: Text('No notes available'));
           }
         },
       ),
     );
   }
 }
-
