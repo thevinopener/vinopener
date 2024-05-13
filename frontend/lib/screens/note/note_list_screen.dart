0e64676db7c76627c57ea0b1566736b44c632b0f
전원빈 <wjssoqls@gachon.ac.kr>
2024-05-13 10:30:37 +0900

    feat: 노트 취소 버튼 추가(#36)

--- frontend/lib/screens/note/note_list_screen.dart
+++ frontend/lib/screens/note/note_list_screen.dart
@@ -9,72 +9,62 @@ import '../../widgets/note/note_list_card_widget.dart';
 import 'note_result_screen.dart';
 
 
-class NoteListScreen extends StatelessWidget {
+class NoteListScreen extends StatefulWidget {
+  @override
+  _NoteListScreenState createState() => _NoteListScreenState();
+}
+
+class _NoteListScreenState extends State<NoteListScreen> {
+  Future<List<WineNoteCard>>? _notesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  void _loadData() {
+    setState(() {
+      _notesFuture = NoteListService.getNoteList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    void addNote() {
-      Navigator.push(
-        context,
-        CupertinoPageRoute(
-          builder: (context) => NoteSearchScreen(),
-        ),
-      );
-    }
-
-    void viewDetail(int id) {
-      Navigator.push(
-        context,
-        CupertinoPageRoute(
-          builder: (context) => NoteResultScreen(id: id),
-        ),
-      );
-    }
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Tasting Note',
-          style: TextStyle(
-            fontSize: AppFontSizes.large,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        title: Text('Tasting Note'),
         actions: [
           IconButton(
-            color: Colors.black,
-            onPressed: addNote,
             icon: Icon(Icons.add),
+            onPressed: () async {
+              await Navigator.push(
+                context,
+                CupertinoPageRoute(builder: (context) => NoteSearchScreen()),
+              );
+              _loadData();  // 노트 추가 후 화면으로 돌아왔을 때 데이터 새로고침
+            },
           )
         ],
-        shape: Border(bottom: BorderSide(color: Colors.grey)),
       ),
       body: FutureBuilder<List<WineNoteCard>>(
-        future: NoteListService.getNoteList(), // 비동기 함수 호출
+        future: _notesFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return CircularProgressIndicator();
           } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
+            return Text('Error: ${snapshot.error}');
           } else if (snapshot.hasData) {
-            final notes = snapshot.data!;
             return ListView.builder(
-              itemCount: notes.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                  child: GestureDetector(
-                    onTap: () => viewDetail(notes[index].id),
-                    child: NoteCard(wineNoteCard: notes[index]),
-                  ),
-                );
-              },
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) => NoteCard(wineNoteCard: snapshot.data![index]),
             );
           } else {
-            return Center(child: Text('No notes available'));
+            return Text('No notes available');
           }
         },
       ),
     );
   }
 }
+
