ae144371bd2c6e6975732b2a5bc5ec82bdfd2476
hanjs0625 <hanjs0625@gmail.com>
2024-05-14 13:55:21 +0900

    fix: 셀러 기반 추천 테이블 갱신시, cellar_wine 데이터가 10개 이하 일 때 오작동하는 부분 수정 (#58)
    본래 cellar_wine의 데이터가 10개 미만일 경우, 부족한 개수는 rate 기반으로 추천하여 채우는 것으로 구현했는데, scheduler에선 그렇게 작동하지 않음을 확인하여 수정하였습니다.

--- backend/vinopener-batch/src/main/java/coffee/ssafy/vinopenerbatch/global/recommendation/RecommendationProcessor.java
+++ backend/vinopener-batch/src/main/java/coffee/ssafy/vinopenerbatch/global/recommendation/RecommendationProcessor.java
@@ -63,8 +63,11 @@ public class RecommendationProcessor {
             List<Tuple> queryResultList = cellarRepositoryQueryImpl.findAllByCellarCount();
 
             int cellarCount = queryResultList.size();
-            if (cellarCount > 10) {
+            int remnantCount = 0;
+            if (cellarCount >= 10) {
                 cellarCount = 10;
+            } else {
+                remnantCount = 10 - cellarCount;
             }
 
             for (int i = 0; i < cellarCount; i++) {
@@ -77,6 +80,18 @@ public class RecommendationProcessor {
                         .build();
 
                 savingEntities.add(recommendationEntity);
+            }
+
+            // 10개가 채워지지 않는다면, rating 기반으로 평점이 높은 순으로 남은 개수를 채운다.
+            if (remnantCount >= 1) {
+                List<WineEntity> additionalList = wineRepository.findAll(Sort.by(Direction.DESC, "rating"));
+                for (int i = 0; i < remnantCount; i++) {
+                    ContentRecommendationEntity recommendationEntity = ContentRecommendationEntity.builder()
+                            .wine(additionalList.get(i))
+                            .contentRecommendationType(ContentRecommendationType.CELLAR)
+                            .build();
+                    savingEntities.add(recommendationEntity);
+                }
             }
             contentRecommendationRepository.saveAll(savingEntities);
 
