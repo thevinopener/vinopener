3ff3c2be602358561e32f22bd5a3cdc3c287973e
전원빈 <wjssoqls@gachon.ac.kr>
2024-05-14 15:36:35 +0900

    fix: 로딩화면 수정(#36)

--- frontend/lib/screens/note/note_list_screen.dart
+++ frontend/lib/screens/note/note_list_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/note/note_search_screen.dart';
 import 'package:frontend/services/note_service.dart';
+import 'package:shimmer/shimmer.dart';
 
 import '../../constants/fonts.dart';
 import '../../models/note_model.dart';
@@ -44,7 +45,7 @@ class _NoteListScreenState extends State<NoteListScreen> {
         title: Text(
           'Tasting Note',
           style: TextStyle(
-            fontSize: AppFontSizes.large,
+            fontSize: AppFontSizes.medium,
             fontWeight: FontWeight.bold,
           ),
         ),
@@ -69,7 +70,7 @@ class _NoteListScreenState extends State<NoteListScreen> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Container(
                 alignment: Alignment.center,
-                child: CircularProgressIndicator());
+                child: NoteListSkeleton(context),);
           } else if (snapshot.hasError) {
             return Container(
                 alignment: Alignment.center,
@@ -98,4 +99,101 @@ class _NoteListScreenState extends State<NoteListScreen> {
       ),
     );
   }
+}
+
+// 스켈레톤 UI
+Widget NoteListSkeleton(BuildContext context) {
+  return Shimmer.fromColors(
+    baseColor: Colors.grey[300]!,
+    highlightColor: Colors.grey[100]!,
+    child: Column(
+      children: [
+        SizedBox(height: 10),
+        Column(
+          children: List.generate(4, (index) {
+            return Padding(
+              padding: EdgeInsets.fromLTRB(10, 3, 10, 3),
+              child: ElevatedButton(
+                onPressed: null,
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // 사진 부분 시작
+                      Flexible(
+                        flex: 3,
+                        child: Container(
+                          width: double.maxFinite,
+                          height: 200,
+                          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(color: Colors.black, width: 1),
+                          ),
+                          child: Container(color: Colors.grey[200]!),
+                        ),
+                      ),
+                      // 사진 부분 끝
+
+                      // 정보 부분 시작
+                      Flexible(
+                        flex: 6,
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                          width: double.maxFinite,
+                          height: double.maxFinite,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Flexible(
+                                child: Container(
+                                  width: double.maxFinite,
+                                  height: 20,
+                                  color: Colors.grey[200]!,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Flexible(
+                                child: Container(
+                                  width: double.maxFinite,
+                                  height: 20,
+                                  color: Colors.grey[200]!,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Flexible(
+                                child: Container(
+                                  width: double.maxFinite,
+                                  height: 20,
+                                  color: Colors.grey[200]!,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      // 정보 부분 끝
+                    ],
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(double.infinity, 150),
+                  backgroundColor: Colors.white,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    side: BorderSide(color: Colors.grey, width: 1),
+                  ),
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                ),
+              ),
+            );
+          }),
+        ),
+      ],
+    ),
+  );
 }
