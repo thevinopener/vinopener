8c9a9d4426166e2a2146a2eb8b1c9ad9e6f69bb2
hanjs0625 <hanjs0625@gmail.com>
2024-05-16 23:55:30 +0900

    fix: 실패한 빌드를 실수로 main에 merge하여, 이전 상태로 돌리기 위한 커밋입니다.

--- backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/tastingnote/service/TastingNoteService.java
+++ backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/tastingnote/service/TastingNoteService.java
@@ -1,5 +1,7 @@
 package com.ssafy.vinopener.domain.tastingnote.service;
 
+import com.ssafy.vinopener.domain.aichat.data.entity.AssistantThreadEntity;
+import com.ssafy.vinopener.domain.aichat.repository.AssistantThreadRepository;
 import com.ssafy.vinopener.domain.tastingnote.data.dto.request.TastingNoteCreateRequest;
 import com.ssafy.vinopener.domain.tastingnote.data.dto.request.TastingNoteUpdateRequest;
 import com.ssafy.vinopener.domain.tastingnote.data.dto.response.TastingNoteGetListResponse;
@@ -8,6 +10,7 @@ import com.ssafy.vinopener.domain.tastingnote.data.mapper.TastingNoteFlavourMapp
 import com.ssafy.vinopener.domain.tastingnote.data.mapper.TastingNoteMapper;
 import com.ssafy.vinopener.domain.tastingnote.exception.TastingNoteErrorCode;
 import com.ssafy.vinopener.domain.tastingnote.repository.TastingNoteRepository;
+import com.ssafy.vinopener.global.aiassistant.AssistantStream;
 import com.ssafy.vinopener.global.exception.VinopenerException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -26,6 +29,8 @@ public class TastingNoteService {
     private final TastingNoteRepository tastingNoteRepository;
     private final TastingNoteMapper tastingNoteMapper;
     private final TastingNoteFlavourMapper tastingNoteFlavourMapper;
+    private final AssistantThreadRepository assistantThreadRepository;
+    private final AssistantStream assistantStream;
 
     /**
      * 테이스팅노트 생성
@@ -52,10 +57,18 @@ public class TastingNoteService {
      * @param userId 유저 ID
      * @return 테이스팅노트 목록
      */
-    @Transactional(readOnly = true)
+    @Transactional
     public List<TastingNoteGetListResponse> getList(
             final Long userId
     ) {
+        AssistantThreadEntity assistantThreadEntity
+                = assistantThreadRepository.findByUserId(userId).orElse(null);
+
+        if (assistantThreadEntity != null) {
+            assistantStream.cleanConversation(assistantThreadEntity.getThreadId());
+        }
+        assistantThreadRepository.deleteAllByUserId(userId);
+
         return tastingNoteRepository.findAllByUserId(userId).stream()
                 .map(tastingNoteMapper::toGetListResponse)
                 .toList();
