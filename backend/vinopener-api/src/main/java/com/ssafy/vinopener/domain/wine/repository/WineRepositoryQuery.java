26a9f44b2d4e6a72cf845781d8de0bf5b82aacfb
hanjs0625 <hanjs0625@gmail.com>
2024-05-16 10:04:39 +0900

    feat: preference 기반 추천에 필요한 쿼리만 추가 (#37)
    쿼리만 추가했습니다. 추가적인 작업 필요.

--- backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/wine/repository/WineRepositoryQuery.java
+++ backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/wine/repository/WineRepositoryQuery.java
@@ -1,10 +1,13 @@
 package com.ssafy.vinopener.domain.wine.repository;
 
 import com.ssafy.vinopener.domain.wine.data.entity.WineEntity;
+import com.ssafy.vinopener.domain.wine.data.entity.enums.WineType;
 import java.util.List;
 
 public interface WineRepositoryQuery {
 
     List<WineEntity> findAllExceptCellar(Long userId);
+
+    List<WineEntity> findAllByTypeExceptCellar(WineType type, Long userId);
 
 }
--- backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/wine/repository/WineRepositoryQueryImpl.java
+++ backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/wine/repository/WineRepositoryQueryImpl.java
@@ -4,8 +4,10 @@ import static com.ssafy.vinopener.domain.cellar.data.entity.QCellarEntity.cellar
 import static com.ssafy.vinopener.domain.wine.data.entity.QWineEntity.wineEntity;
 
 import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.vinopener.domain.wine.data.entity.WineEntity;
+import com.ssafy.vinopener.domain.wine.data.entity.enums.WineType;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -26,6 +28,26 @@ public class WineRepositoryQueryImpl implements WineRepositoryQuery {
                                 .from(cellarEntity)
                                 .where(cellarEntity.user.id.eq(userId))
                 ))
+                .fetch();
+    }
+
+    @Override
+    public List<WineEntity> findAllByTypeExceptCellar(WineType type, Long userId) {
+
+        JPQLQuery<Long> subQuery = JPAExpressions
+                .select(wineEntity.id)
+                .from(wineEntity)
+                .where(wineEntity.id.notIn(
+                        JPAExpressions
+                                .select(cellarEntity.wine.id)
+                                .from(cellarEntity)
+                                .where(cellarEntity.user.id.eq(userId))
+                ));
+
+        return queryFactory
+                .selectFrom(wineEntity)
+                .where(wineEntity.type.in(type)
+                        .and(wineEntity.id.in(subQuery)))
                 .fetch();
     }
 
