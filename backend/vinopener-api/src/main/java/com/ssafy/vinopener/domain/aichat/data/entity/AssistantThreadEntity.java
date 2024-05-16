1c6cf5ff47585312229499a9950a5003523dcf74
hanjs0625 <hanjs0625@gmail.com>
2024-05-17 01:43:57 +0900

    fix: Entity와 Table간의 연결을 위해 수정 시도 (#80)
    push 했을 때 build가 실패하는 것을 확인하였습니다. 이를 해결하기 위해 entity를 수정해보았습니다.

---
+++ backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/aichat/data/entity/AssistantThreadEntity.java
@@ -0,0 +1,44 @@
+package com.ssafy.vinopener.domain.aichat.data.entity;
+
+import com.ssafy.vinopener.domain.user.data.entity.UserEntity;
+import com.ssafy.vinopener.global.common.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "assistant_thread")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+public class AssistantThreadEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "assistant_thread_id")
+    private Long id;
+
+//    @NotNull
+//    @Column(name = "user_id")
+//    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", updatable = false)
+    private UserEntity user;
+
+    @NotNull
+    @Column(name = "thread_id")
+    private String threadId;
+
+}
