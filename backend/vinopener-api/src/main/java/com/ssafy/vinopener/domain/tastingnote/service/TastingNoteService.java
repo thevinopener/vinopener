02cfde728632211d1cf0edf9e9ff352fbbda7ef7
hanjs0625 <hanjs0625@gmail.com>
2024-05-17 08:53:38 +0900

    fix: DB에 저장된 Thread_id에 해당하는 thread가 이미 닫혀버릴 경우가 있어, 이를 수정 (#80)
    간혹, Thread_Id가 assistant 자체적으로 만료되어 닫히게 되고, 해당 thread를 서버측에서 close하려고 할 때 에러가 발생하는 것을 확인하였습니다. 이를 try-catch로 잡아내어 에러가 발생하더라도 무시하고 진행할 수 있도록 수정하였습니다.

--- backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/tastingnote/service/TastingNoteService.java
+++ backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/tastingnote/service/TastingNoteService.java
@@ -14,11 +14,13 @@ import jakarta.persistence.PersistenceContext;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TastingNoteService {
 
     @PersistenceContext
@@ -56,6 +58,18 @@ public class TastingNoteService {
     public List<TastingNoteGetListResponse> getList(
             final Long userId
     ) {
+        AssistantThreadEntity assistantThreadEntity
+                = assistantThreadRepository.findByUserId(userId).orElse(null);
+
+        if (assistantThreadEntity != null) {
+            try {
+                assistantStream.cleanConversation(assistantThreadEntity.getThreadId());
+            } catch (Exception e) {
+                log.info("e : {}", e.toString());
+            }
+
+        }
+        assistantThreadRepository.deleteAllByUserId(userId);
         return tastingNoteRepository.findAllByUserId(userId).stream()
                 .map(tastingNoteMapper::toGetListResponse)
                 .toList();
