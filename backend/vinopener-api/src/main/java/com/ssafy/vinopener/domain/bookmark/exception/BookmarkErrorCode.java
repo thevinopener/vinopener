88d218bdbeafa3981512f4744750198182ced204
phc <phc5403@gmail.com>
2024-05-17 12:55:43 +0900

    refactor: 즐겨찾기, 셀러 중복 생성 시 예외 처리, 도메인별 중복 에러 코드 통일 (#56)

--- backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/bookmark/exception/BookmarkErrorCode.java
+++ backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/bookmark/exception/BookmarkErrorCode.java
@@ -8,10 +8,7 @@ import org.zalando.problem.Status;
 @Getter
 @RequiredArgsConstructor
 public enum BookmarkErrorCode implements ErrorCode {
-    // 즐겨찾기를 조회할 유저 정보가 올바르지 않을 경우.
-    USER_NOT_FOUND(Status.NOT_FOUND, "유저를 찾을 수 없습니다."),
-    // 즐겨찾기에 등록할 와인 정보가 올바르지 않을 경우.
-    WINE_NOT_FOUND(Status.NOT_FOUND, "와인을 찾을 수 없습니다."),
+    BOOKMARK_ALREADY_EXISTS(Status.CONFLICT, "이미 즐겨찾기에 추가된 와인입니다."),
     BOOKMARK_NOT_FOUND(Status.NOT_FOUND, "즐겨찾기 목록을 찾을 수 없습니다."),
     NOT_BOOKMARKED_WINE(Status.NOT_FOUND, "그 와인을 즐겨찾기 한 적 없습니다 ~");
 
