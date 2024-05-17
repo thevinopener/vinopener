680b835ee05b47106aef59d1af2474060d4ada9b
hanjs0625 <hanjs0625@gmail.com>
2024-05-17 15:12:00 +0900

    feat: 와인 조회를 redis를 이용하여 구현 (#37)
    와인 조회 발생시 redis에 wineid, viewCount를 저장하고, 스케쥴러에서 일정 시간마다 viewCount를 읽어 한번에 조회수를 업데이트하고, 조회수 기반 추천 테이블을 갱신합니다.

---
+++ backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/wine/repository/WineViewRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.vinopener.domain.wine.repository;
+
+import com.ssafy.vinopener.domain.wine.data.entity.WineViewEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WineViewRepository extends CrudRepository<WineViewEntity, Long> {
+
+}
---
+++ backend/vinopener-batch/src/main/java/coffee/ssafy/vinopenerbatch/domain/wine/repository/WineViewRepository.java
@@ -0,0 +1,8 @@
+package coffee.ssafy.vinopenerbatch.domain.wine.repository;
+
+import coffee.ssafy.vinopenerbatch.domain.wine.entity.WineViewEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WineViewRepository extends CrudRepository<WineViewEntity, Long> {
+
+}
