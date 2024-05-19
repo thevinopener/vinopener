eaf5f3b96a03be443998163f6b8eb9d9e442695a
hanjs0625 <hanjs0625@gmail.com>
2024-05-20 02:20:59 +0900

    hotfix: 파이프라인 테스트

--- backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/aichat/service/AiChatService.java
+++ backend/vinopener-api/src/main/java/com/ssafy/vinopener/domain/aichat/service/AiChatService.java
@@ -80,6 +80,7 @@ public class AiChatService {
             final AiChatCreateRequest aiChatCreateRequest,
             final Long userId
     ) {
+        //responseState;
         final var requestState = aiChatCreateRequest.state();
 
         final AiChatCreateUserMessageInfo userMessageInfo = AiChatCreateUserMessageInfo.builder()
